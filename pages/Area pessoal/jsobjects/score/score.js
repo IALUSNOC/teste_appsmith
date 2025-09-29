fca7332a79093e938a7e677c85f510984c4e7779
bcardoso <bcardoso@consulai.com>
2025-09-29 18:52:00 +0000

    ambiental

---
+++ pages/Area pessoal/jsobjects/score/score.js
@@ -0,0 +1,28 @@
+export default {
+ option : {
+  title: {
+    text: 'World Population'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {},
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01]
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+  },
+  series: [
+    {
+      name: '2012',
+      type: 'bar',
+      data: [19325, 23438, 31000, 121594, 134141, 681807]
+    }
+  ]
+};
